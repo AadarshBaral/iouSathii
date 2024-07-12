b7fa54eb996f168c9e833cc3460d1a8f95dedb15
AadarshBaral <77476239+AadarshBaral@users.noreply.github.com>
2024-07-12 11:49:19 +0545

    Update fireConfig.ts

--- src/config/fireConfig.ts
+++ src/config/fireConfig.ts
@@ -17,15 +17,7 @@ const firebaseConfig = {
   appId: EXPO_PUBLIC_APP_ID,
   measurementId: EXPO_PUBLIC_MEASUREMENT_ID
 };
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAKThMdv5mG-OkaJrG8hlxNM0cEyEV9OVY",
-//   authDomain: "fir-test-d91ff.firebaseapp.com",
-//   projectId: "fir-test-d91ff",
-//   storageBucket: "fir-test-d91ff.appspot.com",
-//   messagingSenderId: "780080449796",
-//   appId: "1:780080449796:web:5b7159238f54e2c03ee851",
-//   measurementId: "G-JNZ7ZV4PXE"
-// };
+
 export const serverStamp = Timestamp
 const app = initializeApp(firebaseConfig);
 export const FireAuth = initializeAuth(app, {
